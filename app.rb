14c21e10423a3f57bf17931b8ccdd803e240d5d1
Julia Van Cleve <julia@jmv.me>
2016-01-12 01:23:47 -0800

    Output return values to the command line

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ require_relative "lib/transaction"
 # Product.new(title: "Nano Block Empire State Building", price: 49.99, stock: 12)
 # Product.new(title: "LEGO Firehouse Headquarter", price: 199.99, stock: 0)
 
-# Product.all.count # Should return 3
+# puts Product.all.count # Should return 3
 
 # Product.new(title: "LEGO Iron Man vs. Ultron", price: 22.99, stock: 55)
 # Should return DuplicateProductError: 'LEGO Iron Man vs. Ultron' already exists.
@@ -17,50 +17,50 @@ require_relative "lib/transaction"
 # nanoblock = Product.find_by_title("Nano Block Empire State Building")
 # firehouse = Product.find_by_title("LEGO Firehouse Headquarter")
 
-# nanoblock.title # Should return 'Nano Block Empire State Building'
-# nanoblock.price # Should return 49.99
-# nanoblock.stock # Should return 12
-# nanoblock.in_stock? # Should return true
-# firehouse.in_stock? # Should return false
+# puts nanoblock.title # Should return 'Nano Block Empire State Building'
+# puts nanoblock.price # Should return 49.99
+# puts nanoblock.stock # Should return 12
+# puts nanoblock.in_stock? # Should return true
+# puts firehouse.in_stock? # Should return false
 
 # products_in_stock = Product.in_stock
 # Should return an array of all products with a stock greater than zero
-# products_in_stock.include?(nanoblock) # Should return true
-# products_in_stock.include?(firehouse) # Should return false
+# puts products_in_stock.include?(nanoblock) # Should return true
+# puts products_in_stock.include?(firehouse) # Should return false
 
 # CUSTOMERS
 
 # Customer.new(name: "Walter Latimer")
 # Customer.new(name: "Julia Van Cleve")
 
-# Customer.all.count # Should return 2
+# puts Customer.all.count # Should return 2
 
 # Customer.new(name: "Walter Latimer")
 # Should return DuplicateCustomerError: 'Walter Latimer' already exists.
 
 # walter = Customer.find_by_name("Walter Latimer")
 
-# walter.name # Should return "Walter Latimer"
+# puts walter.name # Should return "Walter Latimer"
 
 # TRANSACTIONS
 
 # transaction = Transaction.new(walter, nanoblock)
 
-# transaction.id # Should return 1
-# transaction.product == nanoblock # Should return true
-# transaction.product == firehouse # Should return false
-# transaction.customer == walter # Should return true
+# puts transaction.id # Should return 1
+# puts transaction.product == nanoblock # Should return true
+# puts transaction.product == firehouse # Should return false
+# puts transaction.customer == walter # Should return true
 
-# nanoblock.stock # Should return 11
+# puts nanoblock.stock # Should return 11
 
 # PURCHASES
 
-# walter.purchase(nanoblock)
+# puts walter.purchase(nanoblock)
 
-# Transaction.all.count # Should return 2
+# puts Transaction.all.count # Should return 2
 
 # transaction2 = Transaction.find(2)
-# transaction2.product == nanoblock # Should return true
+# puts transaction2.product == nanoblock # Should return true
 
 # walter.purchase(firehouse)
 # Should return OutOfStockError: 'LEGO Firehouse Headquarter' is out of stock.
