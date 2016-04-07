c9c386b86542b7c0a850684699ff868be1bb6868
zhj930924 <zhj930924@gmail.com>
2016-04-07 06:58:35 -0400

    Revert "Add basic product attributes and check stock"
    This reverts commit e34c908310679212b105d086572b1c37ae72a3d3.

--- lib/product.rb
+++ lib/product.rb
@@ -1,30 +1,15 @@
 class Product
-  attr_reader :title, :price, :stock
+  attr_reader :title
 
   @@products = []
 
   def initialize(options={})
     @title = options[:title]
-    @price = options[:price]
-    @stock = options[:stock]
     add_to_products
   end
 
   def self.all
     @@products
-  end
-
-  def self.find_by_title(title)
-    @@products.each do |product|
-      if product.title == title
-        return product
-      end
-    end
-  end
-
-  # return boolean
-  def in_stock?
-    @stock > 0 ? true : false
   end
 
   private
