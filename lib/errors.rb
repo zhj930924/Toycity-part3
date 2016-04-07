5111c495f6f58750db756f9e675d469276d9116d
zhj930924 <zhj930924@gmail.com>
2016-04-07 05:00:23 -0400

    Add ability to throw error for duplicated data entry

---
+++ lib/errors.rb
@@ -0,0 +1,2 @@
+class DuplicateProductError < StandardError
+end
--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,26 @@
 class Product
+  attr_reader :title
+
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
   end
+
+  private
+
+  def add_to_products
+    # hmm...
+    if @@products.each { |product| product.title == @title }
+      raise DuplicateProductError, "#{@title} already exists."
+    else
+      @@products << self
+    end
+  end
+
 end
